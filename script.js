a4033e1e969c90247e47c67bb461950f9dde6e8e
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-09-23 14:48:19 -0700

    bank loans started

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clickValue = 1
 let totalClicks = 0
 let clickPoints = 0
+let debt = 0
 
 const upgradePrice = 5
 const money = document.getElementById('money')
@@ -20,4 +21,15 @@ shopButton.addEventListener('click', () => {
         clickValue += 1
     }
     money.innerHTML = `${clickPoints}`
+})
+
+const loanButton = document.getElementById('loanButton')
+loanButton.addEventListener('click', () =>{
+    clickPoints += 10
+    debt += 15
+    money.innerHTML = `${clickPoints}`
+    const debtElement = document.createElement("p")
+    debtElement.innerHTML = `${debt}`
+    const bank = document.getElementById('bank')
+    bank.appendChild(debtElement)
 })
