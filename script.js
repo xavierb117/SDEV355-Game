60f4dc04604a39339211a2f7d6e752b0f6d457cf
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-09-23 14:34:00 -0700

    functional shop

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let totalClicks = 0
 let clickPoints = 0
 
 const upgradePrice = 5
-
+const money = document.getElementById('money')
 const clickerButton = document.getElementById('clicker')
 const showTotal = document.getElementById('totalClicks')
 clickerButton.addEventListener('click', () => {
     totalClicks += clickValue
     clickPoints += clickValue
-    showTotal.innerHTML = "You clicked me";
+    showTotal.innerHTML = `${totalClicks}`;
+    money.innerHTML = `${clickPoints}`
 })
 
 const shopButton = document.getElementById('upgrade')
@@ -18,4 +19,5 @@ shopButton.addEventListener('click', () => {
         clickPoints -= upgradePrice
         clickValue += 1
     }
+    money.innerHTML = `${clickPoints}`
 })
